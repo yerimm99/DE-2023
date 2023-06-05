3b17e4e80fbaaf73b7fe5ee9baf169dabc391277
yerim <86309538+yerimm99@users.noreply.github.com>
2023-06-05 17:22:01 +0900

    Update IMDBStudent20190992.java

--- HW3/IMDBStudent20190992.java
+++ HW3/IMDBStudent20190992.java
@@ -56,6 +56,7 @@ public final class IMDBStudent20190992 implements Serializable {
 			}
 		};
 		JavaPairRDD<String, Integer> counts = ones.reduceByKey(f2);
+		JavaRDD<String> result = counts.map(x -> x._1 + " " + x._2);
 		counts.saveAsTextFile(args[1]);
 		spark.stop();
 	}
